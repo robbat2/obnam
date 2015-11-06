9196e6e77d10ba19e5d82b97958a984fe08741b8
Lars Wirzenius <liw@liw.fi>
2015-11-06 11:53:08 +0000

    Prepare 1.18.1 release

--- setup.py
+++ setup.py
@@ -325,7 +325,7 @@ class Check(Command):
 
 
 setup(name='obnam',
-      version='1.18',
+      version='1.18.1',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
