7db29316a7f0c4265e6aae32e20c78bd3427d49e
Lars Wirzenius <liw@liw.fi>
2015-12-21 19:47:43 +0100

    Make forget remove unused chunks before unlocking

--- obnamlib/plugins/forget_plugin.py
+++ obnamlib/plugins/forget_plugin.py
@@ -96,8 +96,8 @@ class ForgetPlugin(obnamlib.ObnamPlugin):
         # Commit or unlock everything.
         self.repo.commit_client(client_name)
         self.repo.commit_chunk_indexes()
-        self.repo.unlock_everything()
         self.repo.remove_unused_chunks()
+        self.repo.unlock_everything()
         self.app.dump_memory_profile('after committing')
 
         self.repo.close()
